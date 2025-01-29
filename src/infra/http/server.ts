a63bae2aafddcf3a2c1c804a11e83415eda58fa3
Heitor Belém <heitorlbelem98@gmail.com>
2025-01-29 00:27:08 -0300

    Creates a sample doc for upload image route

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,10 +1,15 @@
 import { fastifyCors } from '@fastify/cors'
+import { fastifyMultipart } from '@fastify/multipart'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
 import {
   hasZodFastifySchemaValidationErrors,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
+import { uploadImageRoute } from './routes/uploadImage'
 
 const server = fastify()
 server.setValidatorCompiler(validatorCompiler)
@@ -21,6 +26,20 @@ server.setErrorHandler((error, request, reply) => {
   return reply.status(500).send({ message: 'Internal Server Error' })
 })
 server.register(fastifyCors, { origin: '*' })
+server.register(fastifyMultipart)
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Upload Server API Docs',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+server.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+server.register(uploadImageRoute)
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP Server running!')
 })

214a06197f77f5839c878fc09c27c254d806288c
Heitor Belém <heitorlbelem98@gmail.com>
2025-01-28 23:32:17 -0300

    Adds fastify zod validator and serializer compilers

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,7 +1,25 @@
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
 
 const server = fastify()
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.validation,
+    })
+  }
+  // Send error to Sentry/Datadog/Grafana/OTel
+  console.error(error)
+  return reply.status(500).send({ message: 'Internal Server Error' })
+})
 server.register(fastifyCors, { origin: '*' })
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP Server running!')
